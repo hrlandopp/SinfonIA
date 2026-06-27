3aea0c97d5766cca7b0bb85f99faf3276fcb08fd
hrlandopp <hrlandopp@gmail.com>
2026-06-27 00:04:16 -0600

    correccion de errores en el proyecto

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import { PlaybackProvider } from './context/PlaybackContext.jsx'
-import UIPlayground from './sandbox/UIPlayground'
+import App from './App.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <PlaybackProvider>
-      <UIPlayground />
+      <App />
     </PlaybackProvider>
   </React.StrictMode>,
 )
